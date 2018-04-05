41bd8c90ea8be5f8d7dd3ddd7e8a465269e90fe3
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-04-05 11:17:14 +0800

    Add ConsoleTables And Colorful.Console To CLI Project

--- CoffeeNewspaper_CLI/Program.cs
+++ CoffeeNewspaper_CLI/Program.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CoffeeNewspaper_CLI.ConsoleTables;
 
 namespace CoffeeNewspaper_CLI
 {
     class Program
     {
+        public class Something
+        {
+            public Something()
+            {
+                Id = Guid.NewGuid().ToString("N");
+                Name = "Khalid Abuhkameh";
+                Date = DateTime.Now;
+            }
+
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            var table = new ConsoleTable("one", "two", "three");
+            table.AddRow(1, 2, 3)
+                .AddRow("this line should be longer", "yes it is", "oh");
+
+            table.Write();
+            Console.WriteLine();
+
+            var rows = Enumerable.Repeat(new Something(), 10);
+
+            ConsoleTable
+                .From<Something>(rows)
+                .Write(Format.Alternative);
+
+            Console.ReadKey();
         }
     }
 }
