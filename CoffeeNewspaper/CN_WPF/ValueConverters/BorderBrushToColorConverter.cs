6c32456c333e40133e1c866c1b03bc48bace37d7
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-06-15 21:44:32 +0800

    Tag Button complete

---
+++ CoffeeNewspaper/CN_WPF/ValueConverters/BorderBrushToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows.Media;
+using CN_Presentation;
+using CN_Presentation.Utilities;
+
+namespace CN_WPF
+{
+    /// <summary>
+    /// A converter that takes in a <see cref="Brush"/> and returns 
+    /// the Color of that brush
+    /// </summary>
+    public class BorderBrushToColorConverter : BaseValueConverter<BorderBrushToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as SolidColorBrush).Color;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
