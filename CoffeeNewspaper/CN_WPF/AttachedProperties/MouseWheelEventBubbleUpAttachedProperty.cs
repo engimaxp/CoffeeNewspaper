2f45c52cddda7b2dd2aa53cfb272456678be6f09
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-06-18 09:20:46 +0800

    Hook child ScrollView to parent ScrollView event

---
+++ CoffeeNewspaper/CN_WPF/AttachedProperties/MouseWheelEventBubbleUpAttachedProperty.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CN_WPF
+{
+    /// <summary>
+    /// Try to hook the child scroll view to parent scroll view
+    /// </summary>
+    public class MouseWheelEventBubbleUpAttachedProperty:BaseAttachedProperty<MouseWheelEventBubbleUpAttachedProperty,bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(sender is ScrollViewer scrollViewer)) return;
+            if ((bool) e.NewValue)
+            {
+                RoutedEventHandler onLoaded = null;
+                onLoaded = (s, ee) =>
+                {
+                    scrollViewer.Loaded -= onLoaded;
+
+                    //Hook the event
+                    scrollViewer.FindAndActToAllChild<ScrollViewer>((scrollchildview) =>
+                    {
+                        scrollchildview.PreviewMouseWheel += (sss,eee) => PreviewMouseWheel(sss,eee, scrollViewer);
+                    });
+
+                };
+                scrollViewer.Loaded += onLoaded;
+            }
+        }
+
+        private void PreviewMouseWheel(object sender, MouseWheelEventArgs e, ScrollViewer scrollViewer)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                var eventArg =
+                    new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+                    {
+                        RoutedEvent = UIElement.MouseWheelEvent,
+                        Source = sender
+                    };
+                scrollViewer.RaiseEvent(eventArg);
+            }
+        }
+    }
+}
---
+++ CoffeeNewspaper/CN_WPF/Utilities/VisualTreeExtensionClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CN_WPF
+{
+    public static class VisualTreeExtensionClass
+    {
+        public static void FindAndActToAllChild<T>(this FrameworkElement element,Action<T> doAction) where T : FrameworkElement
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
+            var children = new FrameworkElement[childrenCount];
+
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+                children[i] = child;
+                if (child is T)
+                    doAction((T)child);
+            }
+
+            for (int i = 0; i < childrenCount; i++)
+                if (children[i] != null)
+                {
+                    FindAndActToAllChild<T>(children[i], doAction);
+                }
+
+        }
+        public static T FindFirstChild<T>(this FrameworkElement element) where T : FrameworkElement
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
+            var children = new FrameworkElement[childrenCount];
+
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+                children[i] = child;
+                if (child is T)
+                    return (T)child;
+            }
+
+            for (int i = 0; i < childrenCount; i++)
+                if (children[i] != null)
+                {
+                    var subChild = FindFirstChild<T>(children[i]);
+                    if (subChild != null)
+                        return subChild;
+                }
+
+            return null;
+        }
+    }
+}
