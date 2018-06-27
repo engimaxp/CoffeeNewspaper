9654f8cfaf0bc6df82450fe548a91c416afbfa21
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-06-27 19:40:40 +0800

    Basic UI Complete

--- CoffeeNewspaper/CN_Presentation/ViewModel/Application/WorkSpaceViewModel.cs
+++ CoffeeNewspaper/CN_Presentation/ViewModel/Application/WorkSpaceViewModel.cs
@@ -1,8 +1,38 @@
-﻿using CN_Presentation.ViewModel.Base;
+﻿using System.Windows.Input;
+using CN_Presentation.ViewModel.Base;
 
 namespace CN_Presentation.ViewModel.Application
 {
     public class WorkSpaceViewModel:BaseViewModel
     {
+        #region Public Properties
+        /// <summary>
+        /// Side Task Menu Visible or Collapse
+        /// </summary>
+        public bool SideTaskMenuVisible { get; set; } = true;
+
+        #endregion
+
+
+        #region Constructor
+        public WorkSpaceViewModel()
+        {
+            ToggleTaskMenuCommand = new RelayCommand(ToggleTaskMenu);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ToggleTaskMenuCommand { get; set; }
+        #endregion
+
+        #region Private Methods
+
+        private void ToggleTaskMenu()
+        {
+            SideTaskMenuVisible ^= true;
+        }
+        #endregion
     }
 }
