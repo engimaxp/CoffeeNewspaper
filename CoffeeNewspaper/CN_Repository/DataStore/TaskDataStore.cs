a82d1a002af52deeba70cc571256c39b4afc8491
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-09-23 17:30:28 +0800

    single instance watcher

--- CoffeeNewspaper/CN_Repository/DataStore/TaskDataStore.cs
+++ CoffeeNewspaper/CN_Repository/DataStore/TaskDataStore.cs
@@ -198,6 +198,7 @@ namespace CN_Repository
                 async () =>
                     await mDbContext.Tasks
                         .FirstOrDefaultAsync(r => r.TaskId == taskid)
+                    
             );
         }
 
